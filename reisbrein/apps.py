96f1320fdb5142a2438613c7356385af426bad93
Bart Römgens <bart.romgens@gmail.com>
2017-11-10 15:11:54 +0100

    Rename exampleapp to reisbrein

---
+++ reisbrein/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ReisBreinConfig(AppConfig):
+    name = 'reisbrein'
--- website/cron.py
+++ website/cron.py
@@ -37,7 +37,7 @@ class BackupDaily(CronJobBase):
                 '--all',
                 '--natural-foreign',
                 'website',
-                'exampleapp',
+                'reisbrein',
                 stdout=fileout
             )
         with open(filepath, 'rb') as f_in:
