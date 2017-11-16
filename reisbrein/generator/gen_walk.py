b54b7b0ec5ec99b9ee9cbc9f4acb038485a06c9c
Mathijs Romans <github@romansland.nl>
2017-11-16 01:58:12 +0100

    moved walking to walk generator

--- reisbrein/generator/gen_walk.py
+++ reisbrein/generator/gen_walk.py
@@ -1,6 +1,7 @@
 from datetime import timedelta
 from geopy.distance import vincenty
 from reisbrein.segment import Segment, TransportType
+from reisbrein.planner import Point
 from enum import Enum
 
 
@@ -12,18 +13,16 @@ class WalkGenerator:
         START = 0
         END = 1
 
-    def __init__(self):
-        pass
-
     def create_segment(self, start, end, fix):
         distance = vincenty(start.location.gps(), end.location.gps()).meters
         delta_t = timedelta(seconds=distance / WalkGenerator.SPEED)
+        new_point = Point()        
         if fix == WalkGenerator.FixTime.START:
-            end.time = start.time + delta_t
+            new_point = Point(end.location, end.time = start.time + delta_t)
         else:
-            start.time = end.time - delta_t
+            new_point = Point(start.location, start.time = end.time - delta_t)
         segment = Segment(TransportType.WALK, start, end, delta_t.total_seconds()/60)
-        return segment
+        return segment, new_point
 
     def create_edges(self, start, end, edges):
         end.time = start.time + timedelta(minutes=50)
