186dadffa142fee93fa4b6e68127a6cb2a1a5fad
Bart Römgens <bart.romgens@gmail.com>
2017-11-17 21:43:12 +0100

    Fix timeline bgcolor

--- reisbrein/templatetags/reisbrein.py
+++ reisbrein/templatetags/reisbrein.py
@@ -30,3 +30,17 @@ def transport_type_color(transport_type):
     if transport_type in color_map:
         return color_map[transport_type]
     return 'default'
+
+
+@register.simple_tag
+def transport_type_bgcolor(transport_type):
+    color_map = {
+        TransportType.TRAIN.name: 'success',
+        TransportType.WALK.name: 'info',
+        TransportType.CAR.name: 'primary',
+        TransportType.BIKE.name: 'warning',
+        TransportType.WAIT.name: 'danger',
+    }
+    if transport_type in color_map:
+        return color_map[transport_type]
+    return 'default'
