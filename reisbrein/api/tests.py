fddc1250791b544fdb49f8736978b02d9157201b
Mathijs Romans <github@romansland.nl>
2017-11-17 18:56:12 +0100

    Added TomTom api

--- reisbrein/api/tests.py
+++ reisbrein/api/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from website.local_settings import *
+from reisbrein.planner import Location
 from ns_api import *
+from .tomtom import TomTomApi
 
 
 class TestNSAPI(TestCase):
@@ -12,3 +14,14 @@ class TestNSAPI(TestCase):
         if NSAPI_USERNAME:
             stations = self.nsapi.get_stations()
             self.assertGreater(len(stations), 10)
+
+
+class TestTomTom(TestCase):
+
+    def setUp(self):
+        self.tomtom = TomTomApi()
+
+    def testStations(self):
+        begin = Location('Madurodam')
+        end = Location('Martinitoren')
+        self.assertGreater(self.tomtom.travel_time( begin, end ), 8300)
---
+++ reisbrein/api/tomtom.py
@@ -0,0 +1,27 @@
+import requests
+import json
+from website.local_settings import *
+
+
+class TomTomApi:
+
+    BASE_URL = 'https://api.tomtom.com/routing/1/calculateRoute/'
+
+    def travel_time(self, begin, end):
+        begin_gps = begin.gps()
+        end_gps = end.gps()
+        arguments = { 'key': TOMTOM_APIKEY }
+        # headers = {'contentType': 'jsonp'}
+        url = TomTomApi.BASE_URL + \
+                str(begin_gps[0]) + ',' + \
+                str(begin_gps[1]) + ':' + \
+                str(end_gps[0]) + ',' + \
+                str(end_gps[1]) + '/json'
+        response = requests.get(url, arguments)
+        # print(response.url)
+        # print(response.json())
+        foo = json.dumps(response.json())
+        result = json.loads(foo)
+        # print(result['routes'][0]['summary'])
+        time = result['routes'][0]['summary']['travelTimeInSeconds']
+        return time
