e6f28d8bf96b55f555682f447598fe3fb914d5a2
Mathijs Romans <github@romansland.nl>
2017-11-21 17:49:54 +0100

    easier admin

--- reisbrein/admin.py
+++ reisbrein/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from reisbrein.models import UserTravelPlan, UserTravelPreferences
 
-admin.site.register(UserTravelPlan)
-admin.site.register(UserTravelPreferences)
+
+class UserTravelPlanAdmin(admin.ModelAdmin):
+    model = UserTravelPlan
+    list_display = ('user', 'start', 'end')
+
+
+class UserTravelPreferencesAdmin(admin.ModelAdmin):
+    model = UserTravelPreferences
+    list_display = ('user', 'has_bicycle', 'has_car', 'travel_time_importance', 'likes_to_bike', 'show_n_results')
+
+
+admin.site.register(UserTravelPlan, UserTravelPlanAdmin)
+admin.site.register(UserTravelPreferences, UserTravelPreferencesAdmin)
