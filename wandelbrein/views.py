3b237776448969ba9008c4da22214904a579a794
Bart Römgens <bart.romgens@gmail.com>
2018-03-25 17:07:00 +0200

    Add wandelbrein input and result template

--- wandelbrein/views.py
+++ wandelbrein/views.py
@@ -1,14 +1,22 @@
 import datetime
 import time
+
 from django.views.generic import TemplateView
+from django.views.generic.edit import FormView
+from django import forms
+from django.urls import reverse
+
+from datetimewidget.widgets import DateTimeWidget
+
 from reisbrein.generator.gen_common import FixTime
 from reisbrein.views import PlanView
+from reisbrein.models import UserTravelPreferences
 from wandelbrein.planner import WandelbreinPlanner
 
 
-class PlanViewReisbrein(TemplateView):
 
-    template_name = 'reisbrein/plan_results.html'
+class PlanViewReisbrein(TemplateView):
+    template_name = 'wandelbrein/plan_results.html'
 
     def get_context_data(self, start, timestamp, **kwargs):
         user, user_preferences = PlanView.get_user_preferences(self.request)
@@ -34,3 +42,35 @@ class PlanViewReisbrein(TemplateView):
         context['arrive_by'] = fix_time == FixTime.END
         context['results'] = results
         return context
+
+
+class PlanForm(forms.Form):
+    start = forms.CharField(label='Van')
+    date_time_widget = DateTimeWidget(attrs={'id':"yourdatetimeid"}, usel10n=True, bootstrap_version=3)
+    leave = forms.DateTimeField(label='Vertrek', widget=date_time_widget)
+
+
+class PlanInputView(FormView):
+    template_name = 'wandelbrein/plan_input.html'
+    form_class = PlanForm
+
+    def __init__(self):
+        super().__init__()
+        self.start = ''
+        self.timestamp_minutes = 0
+
+    def get_initial(self):
+        initial = super().get_initial()
+        if not self.request.user.is_authenticated:
+            return initial
+        user_preferences, created = UserTravelPreferences.objects.get_or_create(user=self.request.user)
+        initial['start'] = user_preferences.home_address
+        initial['leave'] = datetime.datetime.now()
+        return initial
+
+    def form_valid(self, form):
+        self.start = form.cleaned_data['start']
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('wandel-plan-results', args=(self.start, str(self.timestamp_minutes)))
--- website/urls.py
+++ website/urls.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
@@ -7,6 +6,8 @@ from django.views.generic import TemplateView
 from reisbrein.views import PlanInputView
 from reisbrein.views import PlanViewReisbrein
 from reisbrein.views import UserTravelPreferencesView
+
+from wandelbrein import views as wandelviews
 
 from website.views import RegisterView
 from website.views import UserProfileView
@@ -17,10 +18,11 @@ urlpatterns = [
     url(r'^reisadvies/(?P<start>.+)/(?P<end>.+)/(?P<timestamp>.+)$', PlanViewReisbrein.as_view(), name='plan-results'),
 
     url(r'^about/$', TemplateView.as_view(template_name="website/about.html"), name='about'),
-
     url(r'^travel/preferences/$', login_required(UserTravelPreferencesView.as_view()), name='user-travel-preferences'),
-
     url(r'^userprofile/(?P<pk>[0-9]+)/$', login_required(UserProfileView.as_view()), name='userprofile'),
+
+    url(r'^wandeladvies/$', wandelviews.PlanInputView.as_view(), name='wandel-plan-input'),
+    url(r'^wandeladvies/(?P<start>.+)/(?P<timestamp>.+)$', wandelviews.PlanViewReisbrein.as_view(), name='wandel-plan-results'),
 
     url(r'^register/$', RegisterView.as_view()),  # needed to logout the auto-generated anonymous user
     url(r'^accounts/', include('registration.backends.simple.urls')),  # the django-registration module
