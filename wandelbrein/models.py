685ed3f074016a21505ede93f51b0d24a552f53a
Bart Römgens <bart.romgens@gmail.com>
2018-03-25 15:56:15 +0200

    Add trail model

---
+++ wandelbrein/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Trail(models.Model):
+    title = models.CharField(max_length=500)
+    wandelpagina_id = models.CharField(unique=True, primary_key=True, max_length=100)
+    wandelpagina_url = models.URLField(blank=True, null=True, max_length=500)
+    nswandel_url = models.URLField(blank=True, null=True, max_length=500)
+    begin_lon = models.FloatField()
+    begin_lat = models.FloatField()
+    end_lon = models.FloatField()
+    end_lat = models.FloatField()
+    distance = models.FloatField()
+    gpx = models.TextField(blank=True, default="")
