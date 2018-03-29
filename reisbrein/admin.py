f83bd5a12e56eb502f06bf0395cc15879a5d89ff
Mathijs Romans <github@romansland.nl>
2018-03-29 17:33:52 +0200

    Add action to download csv

--- reisbrein/admin.py
+++ reisbrein/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from reisbrein.models import UserTravelPlan, UserTravelPreferences, Request, TravelTime, ApiCache
 from reisbrein.primitives import TransportType
 from django.conf.locale.en import formats as en_formats
+import csv
+from django.http import HttpResponse
+from io import StringIO
+
 en_formats.DATETIME_FORMAT = "d M Y H:i:s"
 
 class UserTravelPlanAdmin(admin.ModelAdmin):
@@ -21,9 +25,27 @@ class RequestAdmin(admin.ModelAdmin):
 
 class TravelTimeAdmin(admin.ModelAdmin):
 
+    actions = ['download_csv']
+
     @staticmethod
     def transport_type_dutch(travel_time):
         return TransportType(travel_time.transport_type).to_dutch()
+
+    # from https://www.endpoint.com/blog/2012/02/22/dowloading-csv-file-with-from-django
+    @staticmethod
+    def download_csv(self, request, queryset):
+        f = StringIO()
+        writer = csv.writer(f)
+        writer.writerow(['flat', 'flon', 'tlat', 'tlon', 'transport_type_dutch', 'distance', 'time_sec', 'speed', 'datetime_created'])
+
+        for s in queryset:
+            writer.writerow([s.flat, s.flon, s.tlat, s.tlon, TravelTimeAdmin.transport_type_dutch(s), s.distance, s.time_sec, s.speed, s.datetime_created])
+
+        f.seek(0)
+        response = HttpResponse(f, content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=stat-info.csv'
+        return response
+    download_csv.short_description = "Download CSV file for selected stats."
 
     model = TravelTime
     list_display = ('flat', 'flon', 'tlat', 'tlon', 'transport_type_dutch', 'distance', 'time_sec', 'speed', 'datetime_created')
