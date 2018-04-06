e50ec86d6be9d09fc5e7d3d8eb246605c100dce0
Mathijs Romans <github@romansland.nl>
2018-04-07 01:28:34 +0200

    Remove test line

--- wandelbrein/planner.py
+++ wandelbrein/planner.py
@@ -45,7 +45,7 @@ def get_weight(close_to_location, trail):
 
 def get_trail(close_to_location):
     trails = Trail.objects.all()
-    return trails[5]
+    # return trails[5]
     weights = [get_weight(close_to_location, t) for t in trails]
     s = sum(weights)
     if s == 0:
